aa5af402c749dc6fa936d859957d26ea96c1c4c3
kathryn-breslin <kathryn.l.breslin@gmail.com>
2018-09-27 13:51:13 -0500

    finally getting back to friend finder

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friendsList = [
+    {
+        name: 'Catbug',
+        photo: 'https://i.kym-cdn.com/entries/icons/facebook/000/012/812/Catbug_by_sircinnamon-d5riz9k.jpg',
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: 'Impossibear',
+        photo: 'https://static.tvtropes.org/pmwiki/pub/images/bw_-_impossibear_4899.png',
+        scores: [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]        
+    },
+    {
+        name: 'Beth',
+        photo: 'https://vignette.wikia.nocookie.net/bravestwarriors/images/a/a2/Beth.png/revision/latest?cb=20120909024351',
+        scores: [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]        
+    },
+    {
+        name: 'Plum',
+        photo: 'https://pre00.deviantart.net/f8b4/th/pre/f/2013/124/0/f/plum_and_catbug_by_brokenhappiness-d645kuy.png',
+        scores: [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]        
+    },
+    {
+        name: 'Chris',
+        photo: 'https://vignette.wikia.nocookie.net/bravestwarriors/images/3/31/Chris.png/revision/latest?cb=20120909025031',
+        scores: [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]        
+    }
+];
+
+module.exports = friendsList;
